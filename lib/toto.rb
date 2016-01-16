b30bf5e049833e019e38c71bf78383f24cde7e8a
Steve Paulo <stevepaulo@gmail.com>
2016-01-15 22:20:44 -0800

    try to fix hero_image

--- lib/toto.rb
+++ lib/toto.rb
@@ -370,9 +370,7 @@ module Toto
       if !self[:image] || !@config[:media_host]
         ''
       else
-        hero_image = "#{@config[:media_host]}/images/posts/#{self[:date].strftime("%Y")}/#{self[:date].strftime("%m")}/#{self[:image]}"
-        @context[:hero_image] = hero_image
-        hero_image
+        "#{@config[:media_host]}/images/posts/#{self[:date].strftime("%Y")}/#{self[:date].strftime("%m")}/#{self[:image]}"
       end
     end
 
